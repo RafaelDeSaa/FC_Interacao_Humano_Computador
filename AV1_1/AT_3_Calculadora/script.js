cc425af053ebc878b0b4188f85e47037f105e9aa
Rafael de Sá <rafaelmoreiraaraujodesa@gmail.com>
2024-09-22 23:04:33 -0300

    first commit

--- AV1_1/AT_3_Calculadora/script.js
+++ AV1_1/AT_3_Calculadora/script.js
@@ -1,1 +1,9 @@
 const num1 = document.getElementById("number1");
+const num2 = document.getElementById("number2");
+const opc = document.getElementById("opcao");
+const calc = document.getElementById("calc");
+
+function somar(num1,num2){
+    return num1 + num2;
+    }
+    
