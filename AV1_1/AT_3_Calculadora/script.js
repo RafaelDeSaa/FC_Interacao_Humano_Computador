622794e63b3895ff886d67cfc77c7a4f2ed4fd35
Rafael de Sá <rafaelmoreiraaraujodesa@gmail.com>
2024-09-22 23:16:26 -0300

    first commit

--- AV1_1/AT_3_Calculadora/script.js
+++ AV1_1/AT_3_Calculadora/script.js
@@ -5,5 +5,22 @@ const calc = document.getElementById("calc");
 
 function somar(num1,num2){
     return num1 + num2;
+}
+function sub(num1,num2){
+    return num1 - num2;
+}
+function div(num1,num2){
+    if(num1 == 0 || num2 ==0){
+        alert("Impossível dividir por 0")
     }
+    return num1/num2;
+}
+function mult(num1,num2){
+    return num1 * num2;
+}
+calc.addEventListener("click"),()=>{
+
+    const valor1 = parseFloat(num1.value);
     
+}
+
